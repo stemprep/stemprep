7d0c5baaae859f1e16cf933de49d0a463e437c28
Jim Joyce <j.joyce92@gmail.com>
2015-08-24 15:06:50 -0500

    no change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,10 +28,10 @@
 //= require calendarAjax
 //= require moment
 //= require gcal
-//= require fullcalendar
+
 
 //= require eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min
-
+//= require fullcalendar
 //= require calendar
 
 
