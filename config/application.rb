645b665f3144c81ebcd090f382ee623df1f271ac
Jim Joyce <j.joyce92@gmail.com>
2015-09-25 08:58:13 -0500

    no changes

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-# Dotenv::Railtie.load
-HOSTNAME = ENV['HOSTNAME']
 
 module SmuText
   class Application < Rails::Application
