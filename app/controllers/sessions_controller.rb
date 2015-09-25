23384ee4e2d5358d38e79f38dc81fdda232e49be
Jim Joyce <j.joyce92@gmail.com>
2015-09-25 09:15:58 -0500

    no change

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:email])
-    if @user.save && @user.authenticate(params[:password][0])
+    if @user && @user.authenticate(params[:password][0])
       session[:user_id] = @user.id
       today_page_visits.update_view_count
       redirect_to @user.get_home_route
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.create(user_params)
     if @user.save
       session[:user_id] = @user.id
       PageVisit.last.update_all
